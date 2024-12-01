7a708ed011be75bd0e72ca65b6c458b808725b3f
Milly0219 <115720959+Milly0219@users.noreply.github.com>
2024-12-01 21:04:42 +0800

    1201
    設好<a>

--- app.py
+++ app.py
@@ -25,6 +25,7 @@ API_KEY = os.getenv('API_KEY')
 API_URL = os.getenv('API_URL')
 openai.api_key = dotenv_values('.env')["API_KEY2"]
 
+
 # 1. 檢查並建立資料庫
 def initialize_database():
     try:
@@ -286,8 +287,7 @@ def nodify():
             print(f"更新資料庫失敗: {e}")
             return jsonify({"error": f"更新失敗: {e}"}), 500
 
-    return render_template(
-        'nodify.html',
+    return render_template('nodify.html',
         pet_name=session.get('pet_name'),
         lost_time=session.get('lost_date'),
         lost_place=session.get('lost_location'),
@@ -297,6 +297,13 @@ def nodify():
         photo_url=session.get('photo_url'),
     )
 
+@app.route('/map', methods=['GET', 'POST'])
+def map():
+    return render_template('index.html')
+
+@app.route('/adopt', methods=['GET', 'POST'])
+def adopt():
+    return render_template('adopt.html')
 
 @app.route('/api/pet-lost', methods=['GET'])
 def get_lost_pets():
